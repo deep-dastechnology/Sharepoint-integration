a282d13936c95bc6f72b62caf569d807a3b6bc9a
Deep Soni <deep@simformsolutions.com>
2024-08-01 18:17:38 +0530

    Initial Commit

---
+++ Sharepoint/Program.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using Sharepoint.Services;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+using static System.Net.WebRequestMethods;
+
+SharePointService sharePointService = new SharePointService("<clientID>", "<tenantid>", "<clientsecret>");
+
+var accessToken = await sharePointService.GetAccessTokenAsync();
+var data = await sharePointService.GetSharePointSiteDataAsync("<MySharepointURL>");
+Console.WriteLine(JsonConvert.SerializeObject(data));
+var data1 = await sharePointService.GetListItemsAsync("<MySharepointURL>","My Folder");
+await sharePointService.UploadFileAsync("<MySharepointURL>","My Folder","myfile.png");
+Console.Read();
