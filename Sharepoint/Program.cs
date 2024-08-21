1ef1494c5cfe4f517fb6742cce4226a5580ff897
Deep Soni <deep@simformsolutions.com>
2024-08-21 18:13:27 +0530

    Code cleanup and new files

--- Sharepoint/Program.cs
+++ Sharepoint/Program.cs
@@ -1,14 +1,26 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using Sharepoint.Services;
-using System.Net.Http.Headers;
-using System.Text.Json.Serialization;
-using static System.Net.WebRequestMethods;
 
-SharePointService sharePointService = new SharePointService("<clientID>", "<tenantid>", "<clientsecret>");
 
+var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+var sharePointSettings = configuration.GetSection("SharePointSettings");
+string clientId = sharePointSettings["ClientId"];
+string tenantId = sharePointSettings["TenantId"];
+string clientSecret = sharePointSettings["ClientSecret"];
+string sharePointUrl = sharePointSettings["SharePointUrl"];
+string folderName = sharePointSettings["FolderName"];
+string fileName = sharePointSettings["FileName"];
+
+
+SharePointService sharePointService = new SharePointService(clientId, tenantId, clientSecret);
 var accessToken = await sharePointService.GetAccessTokenAsync();
-var data = await sharePointService.GetSharePointSiteDataAsync("<MySharepointURL>");
+var data = await sharePointService.GetSharePointSiteDataAsync(sharePointUrl);
 Console.WriteLine(JsonConvert.SerializeObject(data));
-var data1 = await sharePointService.GetListItemsAsync("<MySharepointURL>","My Folder");
-await sharePointService.UploadFileAsync("<MySharepointURL>","My Folder","myfile.png");
+var data1 = await sharePointService.GetListItemsAsync(sharePointUrl, folderName);
+await sharePointService.UploadFileAsync(sharePointUrl, folderName, fileName);
 Console.Read();
